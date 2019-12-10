8c6b3cb62d2eb44b91e08a16a761f39fb609d1d6
Shwetha <Shwetha@LAPTOP-6GEIL6I9>
2019-12-10 10:50:37 +0530

    created new test class 'SampleGithub'

---
+++ src/qsp/sampleGithub.java
@@ -0,0 +1,15 @@
+package qsp;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import generic.BaseTest;
+
+public class sampleGithub
+{
+	@Test
+	public void testA()
+	{
+		Reporter.log("Hello Good morning!",true);
+	}
+}
